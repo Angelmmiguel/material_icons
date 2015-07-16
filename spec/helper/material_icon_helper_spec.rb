e0df0ab3546ab91dfb01c688c223a7ebd7189a69
Klaus Daniel Hott Vidal <klahott@gmail.com>
2015-07-16 16:35:42 -0300

    missing mi from specs

--- spec/helper/material_icon_helper_spec.rb
+++ spec/helper/material_icon_helper_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
- 
+
 describe MaterialIcons::MaterialIconHelper do
   include MaterialIcons::MaterialIconHelper
 
   context 'Ligatures mode' do
     it 'generate the HTML code for an icon' do
-      expect(mi.face.r90.to_s).to eq '<i class="mi r90">face</i>'
+      expect(mi.face.r90.to_s).to eq '<i class="material-icons r90">face</i>'
       expect(mi.face.css_class('my_class').r90.to_s)
-        .to eq '<i class="mi r90 my_class">face</i>'
+        .to eq '<i class="material-icons r90 my_class">face</i>'
       expect(mi.face.css_class('my_class').style('margin-left: 10px;').r90.to_s)
-        .to eq '<i style="margin-left: 10px;" class="mi r90 my_class">face</i>'
+        .to eq '<i style="margin-left: 10px;" class="material-icons r90 my_class">face</i>'
     end
   end
 
@@ -25,11 +25,11 @@ describe MaterialIcons::MaterialIconHelper do
     end
 
     it 'generate the HTML code for an icon' do
-      expect(mi.face.r90.to_s).to eq '<i class="mi face r90"></i>'
+      expect(mi.face.r90.to_s).to eq '<i class="material-icons face r90"></i>'
       expect(mi.face.css_class('my_class').r90.to_s)
-        .to eq '<i class="mi face r90 my_class"></i>'
+        .to eq '<i class="material-icons face r90 my_class"></i>'
       expect(mi.face.css_class('my_class').style('margin-left: 10px;').r90.to_s)
-        .to eq '<i style="margin-left: 10px;" class="mi face r90 my_class"></i>'
+        .to eq '<i style="margin-left: 10px;" class="material-icons face r90 my_class"></i>'
     end
   end
 end
