35fb714d68fd811ce8caa433c50460e9140618fc
Ángel M <Angelmmiguel@users.noreply.github.com>
2022-03-20 18:41:58 +0100

    [#42] Add the shape method and stop using missing method to set the icon (#43)

--- spec/helper/material_icon_helper_spec.rb
+++ spec/helper/material_icon_helper_spec.rb
@@ -4,17 +4,8 @@ describe MaterialIcons::MaterialIconHelper do
   include MaterialIcons::MaterialIconHelper
   
   it 'generate the HTML code for an icon' do
-    expect(mi.face.r90.to_s).to eq '<i class="material-icons r90">face</i>'
-    expect(mi.face.css_class('my_class').r90.to_s)
+    expect(mi.shape(:face).r90.to_s).to eq '<i class="material-icons r90">face</i>'
+    expect(mi.shape('face').css_class('my_class').r90.to_s)
       .to eq '<i class="material-icons r90 my_class">face</i>'
-    # On minor verions than Rails 4.2.0, the order of class/style inline
-    # attributes changes.
-    if Rails.version < '4.2.0'
-      expect(mi.face.css_class('my_class').style('margin-left: 10px;').r90.to_s)
-        .to eq '<i class="material-icons r90 my_class" style="margin-left: 10px;">face</i>'
-    else
-      expect(mi.face.css_class('my_class').style('margin-left: 10px;').r90.to_s)
-        .to eq '<i style="margin-left: 10px;" class="material-icons r90 my_class">face</i>'
-    end
   end
 end
--- spec/models/material_icon_spec.rb
+++ spec/models/material_icon_spec.rb
@@ -5,16 +5,7 @@ describe MaterialIcon do
   describe 'Icons with existing methods' do
     # Style icon
     describe '#Style' do
-      it 'should set the icon name when the method has no params' do
-        mi = MaterialIcon.new
-        # Set the icon and style
-        res = mi.style
-        # Check icon
-        expect(mi.instance_variable_get('@icon')).to eq 'style'
-        expect(res.class).to eq MaterialIcon
-      end
-
-      it 'should call style when the method has params' do
+      it 'should sets the style' do
         mi = MaterialIcon.new
         css_style = 'margin-top: 10px;'
         # Set the icon and style
@@ -25,17 +16,8 @@ describe MaterialIcon do
     end
 
     # Send icon
-    describe '#send' do
-      it 'should set the icon name when the method has no params' do
-        mi = MaterialIcon.new
-        # Set the icon and style
-        res = mi.send
-        # Check icon
-        expect(mi.instance_variable_get('@icon')).to eq 'send'
-        expect(res.class).to eq MaterialIcon
-      end
-
-      it 'should call to send when the method has params' do
+    describe '#send method' do
+      it 'should call to send method' do
         mi = MaterialIcon.new
         css_style = 'margin-top: 10px;'
         # Set the icon and style
@@ -52,32 +34,6 @@ describe MaterialIcon do
         # Send with arguments
         res2 = mi.send :inspect
         expect(res2.class).to eq String
-        # Only send
-        res3 = mi.send
-        expect(res3.class).to eq MaterialIcon
-      end
-    end
-
-    # Class icon
-    describe '#class_icon' do
-
-      it 'should set the class icon when the method has no params' do
-        mi = MaterialIcon.new
-        # Set the icon and style
-        res = mi.class_icon
-        # Check icon
-        expect(mi.instance_variable_get('@icon')).to eq 'class'
-        expect(res.class).to eq MaterialIcon
-      end
-
-      it 'should set the class icon and style' do
-        mi = MaterialIcon.new
-        css_style = 'margin-top: 10px;'
-        # Set the icon and style
-        mi.class_icon.style css_style
-        # Check icon
-        expect(mi.instance_variable_get('@icon')).to eq 'class'
-        expect(mi.instance_variable_get('@style')).to eq css_style
       end
     end
 
@@ -86,15 +42,15 @@ describe MaterialIcon do
 
       it 'should call to_s instead of executing method_missing' do
         mi = MaterialIcon.new
-        res = mi.face.to_str
+        res = mi.shape(:face).to_str
         expect(res).to eq mi.to_s
       end
 
       it 'the class should works when we use Array.join' do
         mi = MaterialIcon.new
         mi2 = MaterialIcon.new
-        expect([mi.face].join).to eq mi.to_s
-        expect([mi.face, mi2.send].join).to eq "#{mi}#{mi2}"
+        expect([mi.shape(:face)].join).to eq mi.to_s
+        expect([mi.shape(:face), mi2.shape(:send)].join).to eq "#{mi}#{mi2}"
       end
     end
   end
